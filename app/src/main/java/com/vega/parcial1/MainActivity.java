74adb4630c8daa612df24823d0698cd9703f725c
Ronald Vega <ronald.vega48@gmail.com>
2018-05-03 03:19:37 -0600

    XML terminado

---
+++ app/src/main/java/com/vega/parcial1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.vega.parcial1;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
