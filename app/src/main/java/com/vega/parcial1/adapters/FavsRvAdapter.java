22a71b6f18a76648cae92e8ceb83856542bcaab3
Ronald Vega <ronald.vega48@gmail.com>
2018-05-08 05:57:41 -0600

    Final bug fix

--- app/src/main/java/com/vega/parcial1/adapters/FavsRvAdapter.java
+++ app/src/main/java/com/vega/parcial1/adapters/FavsRvAdapter.java
@@ -1,6 +1,7 @@
 package com.vega.parcial1.adapters;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,6 +37,7 @@ public class FavsRvAdapter extends RecyclerView.Adapter<FavsRvAdapter.ViewHolder
     private Context mcontext;
     private boolean favo;
     public List<ModelContacts> mListFavs;
+    Dialog myDialog;
 
     public FavsRvAdapter(Context mcontext, List<ModelContacts> mListFavs) {
         this.mcontext = mcontext;
@@ -49,6 +52,9 @@ public class FavsRvAdapter extends RecyclerView.Adapter<FavsRvAdapter.ViewHolder
         View view = inflater.inflate(R.layout.item_favs, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
 
+        myDialog = new Dialog(mcontext);
+        myDialog.setContentView(R.layout.contact_display);
+
         return viewHolder;
     }
 
@@ -57,10 +63,12 @@ public class FavsRvAdapter extends RecyclerView.Adapter<FavsRvAdapter.ViewHolder
         TextView contact_name, contact_number;
         ImageView boton;
         Button llamando;
+        LinearLayout contacto;
 
         contact_name = holder.contact_name;
         contact_number = holder.contact_number;
         boton = holder.boton;
+        contacto = holder.contacto;
         llamando = holder.llamando;
 
         contact_name.setText(mListFavs.get(position).getName());
@@ -134,6 +142,42 @@ public class FavsRvAdapter extends RecyclerView.Adapter<FavsRvAdapter.ViewHolder
             }
         });
 
+        contacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final TextView tvname = myDialog.findViewById(R.id.display_name);
+                final TextView tvnumber = myDialog.findViewById(R.id.display_number);
+                tvname.setText(mListFavs.get(position).getName());
+                tvnumber.setText(mListFavs.get(position).getNumber());
+                myDialog.show();
+
+                Button llamar2 = myDialog.findViewById(R.id.display_call);
+                Button share = myDialog.findViewById(R.id.display_share);
+                share.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(mcontext, "Compartiendo datos", Toast.LENGTH_SHORT).show();
+
+                        Intent shareIntent = new Intent();
+                        shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+                        shareIntent.setType("*/*");
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, mcontext.getString(R.string.app_nombre) + ": " + mListFavs.get(position).getName().toString() + "\n" + mcontext.getString(R.string.app_numero) + ": " + mListFavs.get(position).getNumber().toString());
+                        mcontext.startActivity(shareIntent);
+
+                    }
+                });
+
+                llamar2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        llamar(mListFavs.get(position).getNumber());
+                    }
+                });
+            }
+
+
+        });
+
     }
 
     @Override
@@ -146,16 +190,21 @@ public class FavsRvAdapter extends RecyclerView.Adapter<FavsRvAdapter.ViewHolder
 
 
         TextView contact_name, contact_number;
-        ImageView boton;
-        Button llamando;
+        ImageView boton, contact_icon;
+        Button llamando, share;
+        LinearLayout contacto;
         boolean fav;
 
         public ViewHolder(View itemView){
             super(itemView);
 
             contact_name = itemView.findViewById(R.id.contact_name);
+            contacto = itemView.findViewById(R.id.desplegar);
             contact_number = itemView.findViewById(R.id.contact_number);
             boton = itemView.findViewById(R.id.favorito);
+
+            contact_icon = itemView.findViewById(R.id.contact_icon);
+            share = itemView.findViewById(R.id.display_share);
             llamando = itemView.findViewById(R.id.llamar);
         }
     }
